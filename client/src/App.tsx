5acaf9c453a5439d152cf19aa98ec0fb3bfecb0d
Mike Miller <chop3593@gmail.com>
2023-03-03 18:12:52 -0700

    Tweaks

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import BeerList from './components/BeerList/BeerList';
 import BreweryList from './components/BreweryList/BreweryList';
+import Header from './components/Header/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <BeerList />
       <BreweryList />
     </div>
---
+++ client/src/components/Header/Header.tsx
@@ -0,0 +1,37 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function Header() {
+  return (
+    <header className="bg-light">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container">
+          <a className="navbar-brand" href="/">
+            Beer Tracker
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <a className="nav-link" href="/breweries">
+                  Breweries
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
