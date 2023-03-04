137e2c2f984709f5e5e5de4686429f57689d79d8
Mike Miller <chop3593@gmail.com>
2023-03-03 20:42:42 -0700

    ROuting

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,15 +3,19 @@ import BreweryList from './components/BreweryList/BreweryList';
 import Header from './components/Header/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <BeerList />
-      <BreweryList />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<BeerList />} />
+          <Route path="/breweries" element={<BreweryList />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
